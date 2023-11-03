1f146757c4ed21e012b142c6186748eea186de69
lekhoa178 <leanhkhoapl178@gmail.com>
2023-11-03 09:15:48 +0700

    text-to-speech feature

--- components/ui/Volume.js
+++ components/ui/Volume.js
@@ -1,8 +1,17 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
+import * as Speech from 'expo-speech';
 
-function Volume() {
-  return <FontAwesome name='volume-up' size={35} color={'#7599FF'} />;
+function Volume({ word }) {
+  function speak() {
+    Speech.speak(word);
+  }
+
+  return (
+    <Pressable onPress={speak}>
+      <FontAwesome name='volume-up' size={35} color={'#7599FF'} />
+    </Pressable>
+  );
 }
 
 export default Volume;
