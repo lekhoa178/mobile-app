500942276b0a437d03d9f4f3e815c5160072b667
hnhan2003 <ngohoangnhan03@gmail.com>
2023-11-04 15:35:29 +0700

    Update add sign up screen

--- components/navigation/RightBarHeader.js
+++ components/navigation/RightBarHeader.js
@@ -5,26 +5,86 @@ import {
   FlatList,
   TextInput,
   Dimensions,
-} from 'react-native';
-import { FontAwesome, FontAwesome5, Ionicons } from '@expo/vector-icons';
-import { useState, useEffect } from 'react';
+  TouchableOpacity,
+  ActivityIndicator
+} from "react-native";
+import { FontAwesome, FontAwesome5, Ionicons } from "@expo/vector-icons";
+import { useState, useEffect } from "react";
+import * as Speech from "expo-speech";
 
 function RightBarHeader() {
+  const [recognizedText, setRecognizedText] = useState("");
+  const [isListening, setIsListening] = useState(false);
+
+  useEffect(() => {
+    async function checkPermissions() {
+      const { status } = await Speech.requestPermissionsAsync();
+      if (status !== "granted") {
+        console.error("Permission to use the microphone is not granted.");
+      }
+    }
+    checkPermissions();
+  }, []);
+
+  const startSpeechToText = async () => {
+    try {
+      setIsListening(true);
+      setRecognizedText("");
+      const result = await Speech.startListeningAsync();
+      if (result.status === "granted") {
+        Speech.onSpeechResults = event => {
+          const transcript = event.value[0];
+          setRecognizedText(transcript);
+        };
+      }
+    } catch (error) {
+      console.error("Error starting speech recognition:", error);
+    }
+  };
+
+  const stopSpeechToText = async () => {
+    try {
+      setIsListening(false);
+      await Speech.stopListeningAsync();
+    } catch (error) {
+      console.error("Error stopping speech recognition:", error);
+    }
+  };
   return (
-    <View style={{ flexDirection: 'row' }}>
-      <FontAwesome5
-        name='microphone-alt'
-        size={24}
-        color='#2499FF'
-        style={{ marginRight: 30 }}
-      />
+    <View style={{ flexDirection: "row" }}>
+      {isListening
+        ? // ? <ActivityIndicator
+          //     size="large"
+          //     color="blue"
+          //     onPress={stopVoiceRecognition}
+          //   />
+          <TouchableOpacity onPress={stopSpeechToText}>
+            <FontAwesome5
+              name="microphone-alt"
+              size={24}
+              color="pink"
+              style={{ marginRight: 30 }}
+            />
+          </TouchableOpacity>
+        : <TouchableOpacity onPress={startSpeechToText}>
+            <FontAwesome5
+              name="microphone-alt"
+              size={24}
+              color="#2499FF"
+              style={{ marginRight: 30 }}
+            />
+          </TouchableOpacity>}
+      {recognizedText !== "" &&
+        <Text>
+          Text đã chuyển đổi: {recognizedText}
+        </Text>}
+      {/* {isListening && <Text style={styles.listeningText}>Đang ghi âm...</Text>} */}
+
       <Ionicons
-        name='search-outline'
+        name="search-outline"
         size={24}
-        color='#2499FF'
-        style={{
-          marginRight: 20,
-        }}
+        color="#2499FF"
+        style={{ marginRight: 20 }}
       />
     </View>
   );
@@ -32,4 +92,14 @@ function RightBarHeader() {
 
 export default RightBarHeader;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  listeningText: {
+    marginTop: 10,
+    color: "blue"
+  }
+});
--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -5,29 +5,26 @@ import {
   TextInput,
   StyleSheet,
   Switch,
-  Pressable
+  Pressable,
+  Button
 } from "react-native";
-import { Navigation } from "react-native-navigation";
+import { Ionicons } from "@expo/vector-icons";
 const marginLeft = 20;
-const LoginScreen = ({ navigation }) => {
+const SignUpScreen = ({ navigation }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [rememberMe, setRememberMe] = useState(false);
 
-  const handleLogin = () => {
-    // Implement your authentication logic here
-    // For simplicity, we'll just log the entered values
-    console.log("Username:", username);
-    console.log("Password:", password);
+  const handleGoback = () => {
+    navigation.goBack();
   };
-  const handleSignUp = () => {
-    navigation.navigate("signup");
-  };
+
+  const handleSignUp = () => {};
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>MONOLINGO</Text>
-      <Text style={styles.textWelcome}>Login to your account !</Text>
+      <Text style={styles.textWelcome}>Sign up to your account !</Text>
       <Text style={styles.label}>Username: </Text>
       <TextInput
         style={styles.input}
@@ -43,26 +40,26 @@ const LoginScreen = ({ navigation }) => {
         value={password}
         onChangeText={text => setPassword(text)}
       />
-      <View style={styles.rememberMe}>
-        <Text style={styles.label}>Remember Me: </Text>
-        <Switch
-          value={rememberMe}
-          onValueChange={value => setRememberMe(value)}
-        />
-      </View>
-      <Pressable
-        onPress={handleLogin}
-        style={styles.loginButton}
-        titleStyle={styles.loginButtonText}
-      >
-        <Text style={styles.textLogin}>Login</Text>
-      </Pressable>
+      <Text style={styles.label}>Repeat password: </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter repeat password"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={text => setPassword(text)}
+      />
+
       <Pressable
         onPress={handleSignUp}
         style={styles.loginButton}
         titleStyle={styles.loginButtonText}
       >
         <Text style={styles.textLogin}>Sign Up</Text>
+      </Pressable>
+
+      <Pressable style={styles.buttonBack} onPress={handleGoback}>
+        <Ionicons name="ios-arrow-back-outline" size={24} color="white" />
+        <Text style={{ textAlign: "center", color: "white" }}>Back</Text>
       </Pressable>
     </View>
   );
@@ -121,7 +118,18 @@ const styles = StyleSheet.create({
   textLogin: {
     textAlign: "center",
     color: "white" // Change the text color
+  },
+  buttonBack: {
+    alignItems: "center",
+    flexDirection: "row",
+    marginLeft: "auto",
+    marginRight: 20,
+    width: "25%",
+    marginTop: 30,
+    backgroundColor: "#2499FF",
+    padding: 7,
+    borderRadius: 20
   }
 });
 
-export default LoginScreen;
+export default SignUpScreen;
